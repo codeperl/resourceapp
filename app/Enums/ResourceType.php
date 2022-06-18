67f72d0a352cc6f40583c2fa2f2e50efa19fcb92
Mohammad Ashraufuddin Ferdousi <ashferd.business@gmail.com>
2022-06-19 05:41:43 +0600

    Resource delete operation is done.

---
+++ app/Enums/ResourceType.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+class ResourceType
+{
+    public const PDF = 'Pdf Download';
+}
--- app/Services/ResourceService.php
+++ app/Services/ResourceService.php
@@ -2,9 +2,10 @@
 
 namespace App\Services;
 
+use App\Enums\ResourceType;
+use App\Enums\Storage as StorageDisks;
 use App\Repositories\ResourceRepository;
 use App\Services\Concerns\Datatable;
-use App\Services\Concerns\Delete;
 use App\Services\Concerns\Detail;
 use App\Services\Concerns\InitiateKlass;
 use App\Services\Concerns\Messages;
@@ -13,11 +14,13 @@ use App\Services\Contracts\Deletable;
 use App\Services\Contracts\Detailable;
 use App\Services\Contracts\InitiableContract;
 use App\Services\Contracts\MessagableContract;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class ResourceService implements Contracts\ResourceContract, InitiableContract, DatatableContract, MessagableContract,
     Detailable, Deletable
 {
-    use InitiateKlass, Messages, Datatable, Detail, Delete;
+    use InitiateKlass, Messages, Datatable, Detail;
 
     /** @var ResourceRepository */
     private $resourceRepository;
@@ -25,5 +28,29 @@ class ResourceService implements Contracts\ResourceContract, InitiableContract, 
     public function __construct(ResourceRepository $resourceRepository)
     {
         $this->resourceRepository = $resourceRepository;
+    }
+
+    public function destroy($repositoryKlass, $entity, $resourceKlass, $message='')
+    {
+        DB::beginTransaction();
+
+        try {
+            $this->repositoryLocator($repositoryKlass)->delete($entity);
+            if($entity->resource_type->name===ResourceType::PDF) {
+                Storage::disk(StorageDisks::ADMIN_UPLOADED_STORAGE)->delete($entity->url);
+            }
+
+            DB::commit();
+
+            return (new $resourceKlass(null))
+                ->additional($this->success($message));
+        } catch(\Exception $e) {
+            DB::rollBack();
+
+            return (new $resourceKlass(null))
+                ->additional($this->failed(
+                    $e->getMessage()
+                ));
+        }
     }
 }
