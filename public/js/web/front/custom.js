46e4a534a4c8f29a12fe7cef3f08c252855e047c
Mohammad Ashraufuddin Ferdousi <ashferd.business@gmail.com>
2022-06-22 01:40:25 +0600

    Text copy fixed.

--- public/js/web/front/custom.js
+++ public/js/web/front/custom.js
@@ -5,13 +5,39 @@ var __webpack_exports__ = {};
   \******************************************/
 window.copyToClipboard = function () {
   var copyText = document.getElementById("code_snippet");
+  copyText.focus();
   copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  navigator.clipboard.writeText(copyText.value);
+  /*copyText.setSelectionRange(0, 99999);*/
+
+  var range = document.createRange();
+  range.selectNode(copyText);
+  window.getSelection().addRange(range);
+  var copyOperationStatus = false;
+  var mssg = 'Can not Copied!';
+
+  if (typeof navigator.clipboard == 'undefined') {
+    try {
+      copyOperationStatus = document.execCommand('copy');
+      console.log(copyOperationStatus);
+    } catch (err) {
+      copyOperationStatus = false;
+    }
+  } else if (typeof navigator.clipboard !== 'undefined') {
+    navigator.clipboard.writeText(copyText.value);
+    copyOperationStatus = true;
+  } else {
+    window.clipboardData.setData("Text", copyText.value);
+  }
+
+  if (copyOperationStatus) {
+    mssg = "Copied!";
+  }
+
   var tooltip = document.getElementById("code_snippet_copy_tooltip");
-  tooltip.innerHTML = "Copied!";
+  tooltip.innerHTML = mssg;
   var copyToClipboardButtonText = document.getElementById("copy_to_clipboard_button_text");
-  copyToClipboardButtonText.innerHTML = "Copied!";
+  copyToClipboardButtonText.innerHTML = mssg;
+  window.getSelection().removeAllRanges();
 };
 
 window.copiedFunc = function () {
--- resources/js/web/front/custom.js
+++ resources/js/web/front/custom.js
@@ -1,13 +1,37 @@
 window.copyToClipboard = function() {
     let copyText = document.getElementById("code_snippet");
+    copyText.focus();
     copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
+
+    const range = document.createRange();
+    range.selectNode(copyText);
+    window.getSelection().addRange(range);
+
+    let copyOperationStatus = false;
+    let mssg = 'Can not Copied!';
+
+    if (typeof (navigator.clipboard) == 'undefined') {
+        try {
+            copyOperationStatus = document.execCommand('copy');
+        } catch (err) {
+            copyOperationStatus = false;
+        }
+    } else if(typeof (navigator.clipboard) !== 'undefined') {
+        navigator.clipboard.writeText(copyText.value);
+        copyOperationStatus = true;
+    } else {
+        window.clipboardData.setData("Text", copyText.value);
+    }
+
+    if(copyOperationStatus) {
+        mssg = "Copied!";
+    }
 
     let tooltip = document.getElementById("code_snippet_copy_tooltip");
-    tooltip.innerHTML = "Copied!";
+    tooltip.innerHTML = mssg;
     let copyToClipboardButtonText = document.getElementById("copy_to_clipboard_button_text");
-    copyToClipboardButtonText.innerHTML = "Copied!";
+    copyToClipboardButtonText.innerHTML = mssg;
+    window.getSelection().removeAllRanges();
 }
 
 window.copiedFunc = function() {
